46d28e18b880fdbf807c32d3c2809530b1ad3032
Markku Rontu <markku.rontu@iki.fi>
2013-04-27 12:27:01 +0300

    Downgrade cljsbuild to make it work with Heroku

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  ;;[cheshire "3.0.0"]
                  [com.keminglabs/singult "0.1.6"]
                  ]
-  :plugins [[lein-cljsbuild "0.3.0"]]
+  :plugins [[lein-cljsbuild "0.2.9"]]
   :hooks [leiningen.cljsbuild]
   :min-lein-version "2.0.0"
   :main finaljourney.server
