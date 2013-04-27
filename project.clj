4f2e75135fa4000693f4d47e6ecdf0a32da93023
Markku Rontu <markku.rontu@iki.fi>
2013-04-27 12:36:11 +0300

    Revert to old ClojureScript dependencies that work with Heroku

--- project.clj
+++ project.clj
@@ -4,20 +4,27 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [crate "0.2.1"]
-                 [enfocus "1.0.1"]
+                 [enfocus "1.0.0-beta2"]
                  [compojure "1.1.1"]
                  ;;[hiccup "1.0.1"]
                  [ring "1.1.6"]
                  ;;[cheshire "3.0.0"]
                  [com.keminglabs/singult "0.1.6"]
                  ]
-  :plugins [[lein-cljsbuild "0.2.9"]]
+  :plugins [[lein-cljsbuild "0.2.7"]]
   :hooks [leiningen.cljsbuild]
   :min-lein-version "2.0.0"
   :main finaljourney.server
-  :cljsbuild {:builds
-              [{:source-paths ["src"],
-                :compiler {:pretty-print true,
-                           :output-to "resources/public/js/cljs.js",
-                           :optimizations :whitespace},
-                :jar true}]})
+  :cljsbuild {
+              :builds [{
+                        :jar true
+                        :source-path "src"
+                        :compiler {
+                                   :output-to "resources/public/js/cljs.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
+  ;; :cljsbuild {:builds [{:source-paths ["src"],
+  ;;                       :compiler {:pretty-print true,
+  ;;                                  :output-to "resources/public/js/cljs.js",
+  ;;                                  :optimizations :whitespace},
+  ;;                       :jar true}]})
--- src/finaljourney/main.cljs
+++ src/finaljourney/main.cljs
@@ -7,6 +7,19 @@
   (when js/console
     (.log js/console (apply str messages))))
 
+(defn clj->js
+  "Recursively transforms ClojureScript maps into Javascript objects,
+   other ClojureScript colls into JavaScript arrays, and ClojureScript
+   keywords into JavaScript strings."
+  [x]
+  (cond
+   (string? x) x
+   (keyword? x) (name x)
+   (map? x) (.-strobj (reduce (fn [m [k v]]
+                                (assoc m (clj->js k) (clj->js v))) {} x))
+   (coll? x) (apply array (map clj->js x))
+   :else x))
+
 (def data (atom {}))
 
 (defn make-stage []
